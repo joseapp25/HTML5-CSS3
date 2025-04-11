62f61382b78c6819937dcc33125fe08825ab6a4e
joseapp25 <joseapp25@gmail.com>
2025-04-11 09:02:51 -0300

    exercises added

--- js-ex-09.js
+++ js-ex-09.js
@@ -1,2 +1,8 @@
-var vel = 60.5
-console.log(`A velocidade do seu carro é de ${vel}Km/h.`)
+var vel = 50
+console.log(`A velocidade do seu carro é de ${vel}km/h.`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitia. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança.`)
---
+++ js-ex-10.js
@@ -0,0 +1,10 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}.`)
+
+if (pais == 'Brasil') { // No lugar de == (igual), tb é possível usar != (diferente).
+    console.log('Brasileiro!')
+}
+
+else {
+    console.log('Estrangeiro!')
+}
